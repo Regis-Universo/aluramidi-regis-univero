113a37d6c22c9497ee914205ee12893cb11d9b4d
Regis <regisuniverso@gmail.com>
2023-10-16 13:52:55 -0300

    Começando com Listas

--- main.js
+++ main.js
@@ -2,14 +2,4 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play()
-}
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play()
-}
-
+document.querySelectorAll('.tecla');
