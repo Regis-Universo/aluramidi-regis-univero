3c50a3a86bd200fa7b0a84ebc70d474001ccb335
Regis <regisuniverso@gmail.com>
2023-10-16 13:18:35 -0300

    First commit

---
+++ REDME.md
@@ -0,0 +1,2 @@
+Repositório criado a partir do curso JavaScript para Web:
+Crie páginas dinãmicas
---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play()
+}
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play()
+}
+
