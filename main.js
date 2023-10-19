d65372ef5d38a879e1c8ae7b8567a2310025b5d0
Regis <regisuniverso@gmail.com>
2023-10-19 11:47:18 -0300

    Adicionando Funções ao main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector('idElementoAudio').play();
 }
 
 // Criando uma REFERÊNCIA CONSTANTE
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
